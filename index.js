f9725a53c3985496b487dd7ee1a0195e58d81d69
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-01-29 22:29:45 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 14;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+const numbers = [10, 5, 20, 15, 8];
+let max = -Infinity;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > max) {
+    max = numbers[i];
+  }
+}
+
